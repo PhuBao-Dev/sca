51feee6ebb552959c273f380136bdb01fbe714d2
tranbaophu <92238818+tranbaophu@users.noreply.github.com>
2022-04-15 09:42:56 +0700

    fc

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 @app.route('/', methods=('GET', 'POST'))
 def index():
     if request.method == 'POST':
-        username = request.form['username']
+        sentence = request.form['sentence']
         checker = SpellChecker("./movies_text.txt")
-        sen = username.split()
+        sen = sentence.split()
         rel = []
         for wo in sen:
             rel.append(checker.check(wo)[0][0])
         
         result = " ".join(rel)
-        return render_template('index.html', result=result, getIn=username)
+        return render_template('index.html', result=result, getIn=sentence)
 
     return render_template('index.html')
 
